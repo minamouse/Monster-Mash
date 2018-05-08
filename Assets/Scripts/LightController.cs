f6f82278c635378d4158e8d9de353f22ef36380a
David Cottrell <davidrcottrell@gmail.com>
2018-05-08 01:25:14 -0700

    more stuff

--- Assets/Scripts/LightController.cs
+++ Assets/Scripts/LightController.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class LightController : MonoBehaviour {
 
     public Light lt;
-	// Use this for initialization
-	void Start () {
+
+    TGCConnectionController controller;
+    private int meditation;
+
+    void Start () {
         lt = GetComponent<Light>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+        controller = GameObject.Find("NeuroSkyTGCController").GetComponent<TGCConnectionController>();
+        controller.UpdateMeditationEvent += OnUpdateMeditation;
         //change this as 
-        lt.intensity = 1f;
+        //lt.intensity = meditation / 50;
+        //lt.color = Color.Lerp(Color.black, Color.white, meditation / 100);
 	}
+    void OnUpdateMeditation(int value)
+    {
+        meditation = value;
+    }
 }
--- Assets/Scripts/MainCharacter.cs
+++ Assets/Scripts/MainCharacter.cs
@@ -7,48 +7,89 @@ public class MainCharacter : MonoBehaviour {
     Animator animator;
     int jumpHash = Animator.StringToHash("Jump");
     float goal = 0.0f;
+    float yGoal = 0.0f;
 
     public GameObject goblinPrefab;
     public GameObject insectPrefab;
     public GameObject trollPrefab;
+    public GameObject coin;
+
     int stopHash = Animator.StringToHash("Stop Game");
     int startHash = Animator.StringToHash("Start Game");
     bool gameOver = true;
     Vector3 startPos = new Vector3(0, 0, -5.93f);
 
+    public List<GameObject> monsterTypes = new List<GameObject>();
+    public List<float> positions = new List<float>();
+    public HashSet<GameObject> monsters = new HashSet<GameObject>();
+    public List<GameObject> coins = new List<GameObject>();
+
+    public int points = 0;
+
     TGCConnectionController controller;
     private int attention;
-    private int meditation;
 
     public CameraController myCamera;
+    float distance;
 
 
     void Start () {
+        monsterTypes.Add(goblinPrefab);
+        monsterTypes.Add(insectPrefab);
+        monsterTypes.Add(trollPrefab);
+
+        positions.Add(-0.6f);
+        positions.Add(0.0f);
+        positions.Add(0.6f);
+
         animator = GetComponent < Animator >();
-        Debug.Log(myCamera);
+        float distance = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         controller = GameObject.Find("NeuroSkyTGCController").GetComponent<TGCConnectionController>();
-        controller.UpdateMeditationEvent += OnUpdateMeditation;
         controller.UpdateAttentionEvent += OnUpdateAttention;
 
         if (gameOver) {
+            foreach (GameObject monsterClone in monsters) {
+                Destroy(monsterClone);
+            }
+            monsters = new HashSet<GameObject>();
             if (Input.GetKeyDown(KeyCode.Space)){
                 gameOver = false;
                 animator.SetTrigger(startHash);
-                Instantiate(insectPrefab, transform.position + new Vector3(0, 0, 10), Quaternion.Euler(0, 180, 0));
             }
         } else {
             transform.Translate(Vector3.forward * Time.deltaTime * 3);
-            Vector3 d = new Vector3(goal - transform.position.x, 0.0f, 0.0f);
+            yGoal = attention / 50;
+            Vector3 d = new Vector3(goal - transform.position.x, yGoal-transform.position.y, 0.0f);
             float step = Time.deltaTime * 3;
+            distance += step;
 
-            if (transform.position.x != goal)
-            {
-                transform.position = Vector3.MoveTowards(transform.position, transform.position + d, step);
+            if (distance >= 3.0f) {
+                float rand = Random.value;
+
+
+                if (rand <= ((attention/100)+0.5)) {
+                    
+                    GameObject monster = monsterTypes[(int)Random.Range(0, 2.9f)];
+                    Vector3 monsterPos = new Vector3(positions[(int)Random.Range(0, 2.9f)], 0, transform.position.z + 20);
+                    GameObject monsterClone = Instantiate(monster, monsterPos, Quaternion.Euler(0, 180, 0));
+                    monsters.Add(monsterClone);
+
+                } else if (rand > ((attention/100)+0.5)) {
+                    
+                    Vector3 coinPos = new Vector3(positions[(int)Random.Range(0, 2.9f)], 0.4f, transform.position.z + 20);
+                    GameObject thisCoin = Instantiate(coin, coinPos, Quaternion.Euler(0, 180, 0));
+                    coins.Add(thisCoin);
+
+                }
+                distance = 0.0f;
             }
+
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + d, step);
+
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
@@ -77,19 +118,22 @@ public class MainCharacter : MonoBehaviour {
     {
         attention = value;
     }
-    void OnUpdateMeditation(int value)
-    {
-        meditation = value;
-    }
+
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == "Monster")
         {
-            
             gameOver = true;
             animator.SetTrigger(stopHash);
             transform.position = startPos;
             myCamera.endGame();
+            points = 0;
+        }
+
+        if (col.gameObject.tag == "Coin")
+        {
+            points += 1;
+
         }
     }
 }
